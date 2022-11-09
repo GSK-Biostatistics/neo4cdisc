61656713b3015c431c637ae794cd6956aca97fc9
Pranjali Parnerkar <pranjaliparnerkar28@gmail.com>
2022-11-09 15:27:53 +0000

    Updated installation.sh

--- installation.bat
+++ installation.bat
@@ -1,6 +1,6 @@
 pip install --upgrade pip
 python -m venv ./venv
 .\venv\Scripts\activate
-venv/bin/python -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 
--- installation.sh
+++ installation.sh
@@ -1,1 +1,4 @@
+pip install --upgrade pip
+python -m venv ./venv
+.\venv\Scripts\activate
 venv/bin/python -m pip install -r requirements.txt
