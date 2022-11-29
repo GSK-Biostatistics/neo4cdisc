06cb24896fec59c71a504dd776e1777289920842
Alexey Kuznetsov <alexey.k.kuznetsov@gsk.com>
2022-11-29 10:05:42 +0000

    fix setenv

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,4 @@
 export NEO4J_HOST=neo4j://127.0.0.1:7687/
 export NEO4J_USER=neo4j
-export NEO4J_PASSWORD=<your password>
+export NEO4J_PASSWORD=your_password
 export NEO4J_RDF_HOST=http://127.0.0.1:7474/rdf/
