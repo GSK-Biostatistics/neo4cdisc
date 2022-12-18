8efea3f964f5d4c50709016b7070a5efd58b633e
Alexey Kuznetsov <paltusplintus@gmail.com>
2022-12-18 13:13:33 +0100

    implemented another example notebook

--- setenv.sh
+++ setenv.sh
@@ -2,3 +2,4 @@ export NEO4J_HOST=neo4j://127.0.0.1:7687/
 export NEO4J_USER=neo4j
 export NEO4J_PASSWORD=your_password
 export NEO4J_RDF_HOST=http://127.0.0.1:7474/rdf/
+export CDISC_LIB_KEY=library_key
--- setup.py
+++ setup.py
@@ -30,14 +30,14 @@ for line in requirements:
 
 setuptools.setup(
     name="neo4cdisc",                           # This is the name of the package
-    version="1.0.1.1",                      # Release.Major Feature.Minor Feature.Bug Fix
+    version="1.0.2.0",                      # Release.Major Feature.Minor Feature.Bug Fix
     author="Alexey Kuznetsov",              # Full name of the author
     description="Clinical Linked Data: Example of loading the FDA CDISC pilot study into Neo4J using the tab2neo python package",
     #long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(include=[
         "cdisc_model_managers",
-        "cdisc_data_providers",
+        "cdisc_data_providers",        
     ]),    # List of all python modules to be installed
     classifiers=[
         "Programming Language :: Python :: 3",
